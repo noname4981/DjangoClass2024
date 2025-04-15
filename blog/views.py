c72170ad84b9fdbb77712dbfa2483c9fa3be8edf
unknown <wnsduq1123@naver.com>
2025-04-15 17:38:26 +0900

    0415 정적파일관리

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    #template_name = 'blog/index.html'
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/single_post_page.html'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
 # def single_post_page(request, pk):
 #     post = Post.objects.get(pk=pk)
