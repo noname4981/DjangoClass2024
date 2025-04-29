a237145f71b6e35c4cc44c97209a9f1f7de72b04
123 <123@asd.asd>
2025-04-29 16:04:10 +0900

    미디어 파일 관리

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     #path('<int:pk>/', views.single_post_page),
     #path('', views.index),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
